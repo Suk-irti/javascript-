5ded92e976b36e866d3877aae78b805fc421ebbd
Suk-irti <162359869+Suk-irti@users.noreply.github.com>
2024-03-19 13:29:16 +0000

    container commit done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+console.log("Sukirti")
+const names = "Audis"
+let accountid = "123cd"
+var accountname="music"
+abc= "123k"
+var accountclass;
+//names= 123
+//const cannot be changed
+//let and var cam be changed but var have scope problem that is it can be changed outside the function.
+acoountid="0000"
+accountname="name"
+console.table([names, accountid, accountname, abc, accountclass])
